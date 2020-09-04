b7b1df960f78d138284f5a18215f3f9a7fb64ed6
AlexZandr87Russia <alex@biofam.ru>
2020-09-04 16:32:21 +0500

    04/09_work_index_1

--- index.php
+++ index.php
@@ -5,6 +5,20 @@ $day = strftime('%d');
 $month = strftime('%m');
 $month = iconv('windows-1251', 'utf-8', $month);
 $year = strftime('%Y');
+$time = date("H:i:s");
+if($month=="01") $mon="января";
+if($month=="02") $mon="февраля";
+if($month=="03") $mon="марта";
+if($month=="04") $mon="ппреля";
+if($month=="05") $mon="мая";
+if($month=="06") $mon="июня";
+if($month=="07") $mon="июля";
+if($month=="08") $mon="августа";
+if($month=="09") $mon="сентября";
+if($month=="10") $mon="октября";
+if($month=="11") $mon="ноября";
+if($month=="12") $mon="декабря";
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,7 +43,7 @@ $year = strftime('%Y');
     <h1>Добро пожаловать на наш сайт!</h1>
     <!-- Заголовок -->
       <blockquote>
-          <?= "Сегодня $day число, $month месяц, $year год." ?>
+          <?= "Сегодня $day $mon $year года. Время: $time" ?>
       </blockquote>
     <!-- Область основного контента -->
     <h3>Зачем мы ходим в школу?</h3>
