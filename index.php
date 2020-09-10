464ac605ce13313bbc16b4111cbead1fce641098
AlexZandr87Russia <alex@biofam.ru>
2020-09-10 18:00:33 +0500

    10/09_work_index_1

--- index.php
+++ index.php
@@ -83,16 +83,25 @@ switch ($hour) {
     <!-- Навигация -->
     <h2>Навигация по сайту</h2>
     <!-- Меню -->
+      <?php
+      $leftMenu = [
+              ['href' => 'index.php', 'link' => 'Домой'],
+              ['href' => 'about.php', 'link' => 'О нас'],
+              ['href' => 'contact.php', 'link' => 'Контакты'],
+              ['href' => 'table.php', 'link' => 'Таблица умножения'],
+              ['href' => 'calc.php', 'link' => 'Калькулятор']
+      ];
+      ?>
     <ul>
-      <li><a href='index.php'>Домой</a>
+      <li><a href='<?=$leftMenu[0]['href']?>'><?=$leftMenu[0]['link']?></a>
       </li>
-      <li><a href='about.php'>О нас</a>
+      <li><a href='<?=$leftMenu[1]['href']?>'><?=$leftMenu[1]['link']?></a>
       </li>
-      <li><a href='contact.php'>Контакты</a>
+      <li><a href='<?=$leftMenu[2]['href']?>'><?=$leftMenu[2]['link']?></a>
       </li>
-      <li><a href='table.php'>Таблица умножения</a>
+      <li><a href='<?=$leftMenu[3]['href']?>'><?=$leftMenu[3]['link']?></a>
       </li>
-      <li><a href='calc.php'>Калькулятор</a>
+      <li><a href='<?=$leftMenu[4]['href']?>'><?=$leftMenu[4]['link']?></a>
       </li>
     </ul>
     <!-- Меню -->
